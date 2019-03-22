034d55ae806fa4f9649f184199f47c16df6537b5
rkalagara <rama.kalagara@gmail.com>
2019-03-22 14:34:10 -0600

    Modified the file

---
+++ gitdemo/src/com/rama/samples/gitdemo/MyGitDemo.java
@@ -0,0 +1,12 @@
+package com.rama.samples.gitdemo;
+
+public class MyGitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is my first git demo");
+		System.out.println("This is my first git demo change file");
+	}
+
+}
